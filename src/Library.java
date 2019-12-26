6bb6b534cade139b7073c6c39443c2bbf96382e0
Tomek <draganiktomasz@gmail.com>
2019-12-26 17:38:07 +0100

    pierwsze dane

--- src/Library.java
+++ src/Library.java
@@ -1,5 +1,24 @@
+import java.sql.SQLOutput;
+
 public class Library {
     public static void main(String[] args) {
-        System.out.println("Biblioteka");
+        final String appName = "Biblioteka v1.0";
+
+        String title = "W pustyni i w puszczy";
+        String author = "Henryk Sienkiewicz";
+        int releasedate = 2010;
+        int pages = 296;
+        String publisher = "Greg";
+        String isbn = "353453346344535";
+
+        System.out.println(appName);
+        System.out.println("Książki dostąpne w bibliotece: ");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releasedate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+
     }
 }
