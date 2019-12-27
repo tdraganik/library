e74d8fe19f12e437ab543b4ddff65f0db897a30f
Tomek <draganiktomasz@gmail.com>
2019-12-27 01:51:22 +0100

    wersja 2, dodanie class Book i obiektu tej klasy

---
+++ src/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+
+    String title;
+    String author;
+    int releasedate;
+    int pages;
+    String publisher;
+    String isbn;
+}
--- src/Library.java
+++ src/Library.java
@@ -1,24 +1,25 @@
-import java.sql.SQLOutput;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v1.0";
+        final String appName = "Biblioteka v2.0";
 
-        String title = "W pustyni i w puszczy";
-        String author = "Henryk Sienkiewicz";
-        int releasedate = 2010;
-        int pages = 296;
-        String publisher = "Greg";
-        String isbn = "353453346344535";
+        Book book1 = new Book();
+
+        book1.title = "W pustyni i w puszczy";
+        book1.author = "Henryk Sienkiewicz";
+        book1.releasedate = 2010;
+        book1.pages = 296;
+        book1.publisher = "Greg";
+        book1.isbn = "353453346344535";
 
         System.out.println(appName);
         System.out.println("Książki dostąpne w bibliotece: ");
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(releasedate);
-        System.out.println(pages);
-        System.out.println(publisher);
-        System.out.println(isbn);
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.releasedate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
 
     }
 }
