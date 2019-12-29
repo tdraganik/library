b8912e7625ba593c883a336a397bb190807f9b68
Tomek <draganiktomasz@gmail.com>
2019-12-29 15:27:33 +0100

    wersja 3, konstruktor dodawania książek

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,4 @@
-public class Book {
+class Book {
 
     String title;
     String author;
@@ -6,4 +6,19 @@ public class Book {
     int pages;
     String publisher;
     String isbn;
+
+    Book(String bookTitle, String bookAuthor,  int bookReleaseDate,
+         int bookPages, String bookPublisher, String bookIsbn) {
+
+        title = bookTitle;
+        author = bookAuthor;
+        releasedate = bookReleaseDate;
+        pages = bookPages;
+        publisher = bookPublisher;
+        isbn = bookIsbn;
+    }
+     void printInfo() {
+        String info = title + "; " + author + "; " + releasedate + "; " + pages + "; " + publisher + "; " + isbn;
+         System.out.println(info);
+    }
 }
